55c4e659dd32a937de58ff926d1c081946989503
Veer Patel <veerpatel3024@gmail.com>
2026-09-10 15:45:20 +1200

    Added JWT refresh token endpoint to authentication urls

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('me/', views.get_current_user, name='current_user'),
     path('push-token/', views.save_push_token, name='save_push_token'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
